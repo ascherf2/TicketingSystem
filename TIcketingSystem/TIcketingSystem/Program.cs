69b9d5d432c71f90cd5f25b7adf1d6e6d2221f57
Allie Scherf <ascherf2@my.wctc.edu>
2022-01-30 11:23:48 -0600

    refactored file for readability

--- TIcketingSystem/TIcketingSystem/Program.cs
+++ TIcketingSystem/TIcketingSystem/Program.cs
@@ -5,6 +5,94 @@ namespace TicketingSystem
 {
     class Program
     {
+        public static void readdata(string file)
+
+        {
+            // Read data from file
+            if (File.Exists(file))
+            {
+
+
+                // read data from file
+                StreamReader sr = new StreamReader(file);
+                while (!sr.EndOfStream)
+                {
+                    string line = sr.ReadLine();
+                    // convert string to array
+
+                    string[] arr = line.Split('|');
+                    // display array data
+
+                    Console.WriteLine("Ticket ID: {0}, Summary: {1}, Status: {2}, Priority: {3}, Submitter: {4}, Assigned: {5}, Watching: {6}", arr[0], arr[1], arr[2], arr[3], arr[4], arr[5], arr[6]);
+
+
+                    sr.Close();
+                }
+            }
+        }
+        public static void createfile(string file)
+        {
+            // create file from data
+            StreamWriter sw = new StreamWriter(file);
+            for (int i = 0; i < 7; i++)
+            {
+                // ask a question
+                Console.WriteLine("Would you like to enter a ticket? (Y/N)?");
+
+                // input the response
+                string resp = Console.ReadLine().ToUpper();
+
+                // if the response is anything other than "Y", stop asking
+                if (resp != "Y") { break; }
+
+                // prompt for Ticket ID
+                Console.WriteLine("Enter ticket ID");
+
+                // save the Ticket ID
+                string ticketid = Console.ReadLine();
+
+                // prompt for summary
+                Console.WriteLine("Enter a summary: ");
+
+                // save the summary
+                string summary = Console.ReadLine().ToUpper();
+
+                // prompt for status
+                Console.WriteLine("Enter the ticket status: ");
+
+                // save the status
+                string status = Console.ReadLine().ToUpper();
+
+                // prompt for priority
+                Console.WriteLine("Enter the priority level of the ticket: ");
+
+                // save the priority
+                string priority = Console.ReadLine().ToUpper();
+
+                // prompt for submitter
+                Console.WriteLine("Enter the submitter for the ticket: ");
+
+                // save the submitter
+                string submitter = Console.ReadLine().ToUpper();
+
+                // prompt for assigned
+                Console.WriteLine("Enter who is assigned to the ticket:  ");
+
+                // save the assigned
+                string assigned = Console.ReadLine().ToUpper();
+
+                // prompt for watching
+                Console.WriteLine("Enter who is watching the ticket: ");
+
+                // save the watching
+                string watching = Console.ReadLine().ToUpper();
+
+
+                sw.WriteLine("{0}|{1}|{2}|{3}|{4}|{5}", ticketid, summary, status, priority, submitter, assigned, watching);
+            }
+            sw.Close();
+
+        }
         static void Main(string[] args)
         {
             string file = "tickets.csv";
@@ -21,99 +109,25 @@ namespace TicketingSystem
 
                 if (choice == "1")
                 {
-                    // Read data from file
-                    if (File.Exists(file))
-                    {
+                    readdata(file);
+
+                }
+
+                else if (choice == "2")
+                {
+                    createfile(file);
 
 
-                        // read data from file
-                        StreamReader sr = new StreamReader(file);
-                        while (!sr.EndOfStream)
-                        {
-                            string line = sr.ReadLine();
-                            // convert string to array
-
-                            string[] arr = line.Split('|');
-                            // display array data
-
-                            Console.WriteLine("Ticket ID: {0}, Summary: {1}, Status: {2}, Priority: {3}, Submitter: {4}, Assigned: {5}, Watching: {6}", arr[0], arr[1], arr[2], arr[3], arr[4], arr[5], arr[6]);
+                }
+                else
+                {
+                    Console.WriteLine("File does not exist");
+                }
 
 
-                            sr.Close();
-                        }
-                    }
-
-
-                    else
-                    {
-                        Console.WriteLine("File does not exist");
-                    }
-                    
-                    else if (choice == "2")
-                    {
-                        // create file from data
-                        StreamWriter sw = new StreamWriter(file);
-                        for (int i = 0; i < 7; i++)
-                        {
-                            // ask a question
-                            Console.WriteLine("Would you like to enter a ticket? (Y/N)?");
-
-                            // input the response
-                            string resp = Console.ReadLine().ToUpper();
-
-                            // if the response is anything other than "Y", stop asking
-                            if (resp != "Y") { break; }
-
-                            // prompt for Ticket ID
-                            Console.WriteLine("Enter ticket ID");
-
-                            // save the Ticket ID
-                            string ticketid = Console.ReadLine();
-
-                            // prompt for summary
-                            Console.WriteLine("Enter a summary: ");
-
-                            // save the summary
-                            string summary = Console.ReadLine().ToUpper();
-
-                            // prompt for status
-                            Console.WriteLine("Enter the ticket status: ");
-
-                            // save the status
-                            string status = Console.ReadLine().ToUpper();
-
-                            // prompt for priority
-                            Console.WriteLine("Enter the priority level of the ticket: ");
-
-                            // save the priority
-                            string priority = Console.ReadLine().ToUpper();
-
-                            // prompt for submitter
-                            Console.WriteLine("Enter the submitter for the ticket: ");
-
-                            // save the submitter
-                            string submitter = Console.ReadLine().ToUpper();
-
-                            // prompt for assigned
-                            Console.WriteLine("Enter who is assigned to the ticket:  ");
-
-                            // save the assigned
-                            string assigned = Console.ReadLine().ToUpper();
-
-                            // prompt for watching
-                            Console.WriteLine("Enter who is watching the ticket: ");
-
-                            // save the watching
-                            string watching = Console.ReadLine().ToUpper();
-
-
-                            sw.WriteLine("{0}|{1}|{2}|{3}|{4}|{5}", ticketid, summary, status, priority, submitter, assigned, watching);
-                        }
-                        sw.Close();
-                    }
-                } while (choice == "1" || choice == "2") ;
-            }
+            } while (choice == "1" || choice == "2");
         }
     }
 }
-    
+
+
