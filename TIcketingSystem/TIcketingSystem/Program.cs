77ca35b9b1c848cd5d25120c24be08befe2956d1
Allie Scherf <ascherf2@my.wctc.edu>
2022-01-30 11:36:01 -0600

    array fixes

--- TIcketingSystem/TIcketingSystem/Program.cs
+++ TIcketingSystem/TIcketingSystem/Program.cs
@@ -88,7 +88,7 @@ namespace TicketingSystem
                 string watching = Console.ReadLine().ToUpper();
 
 
-                sw.WriteLine("{0}|{1}|{2}|{3}|{4}|{5}", ticketid, summary, status, priority, submitter, assigned, watching);
+                sw.WriteLine("{0}|{1}|{2}|{3}|{4}|{5}|{6}", ticketid, summary, status, priority, submitter, assigned, watching);
             }
             sw.Close();
 
